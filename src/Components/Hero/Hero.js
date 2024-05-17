9e7a850790b25f41f2387368b4ad2419e52a8f21
Henry Yum <henry.yum1993@hotmail.com>
2024-05-17 11:00:01 +1000

    updated resume

--- src/Components/Hero/Hero.js
+++ src/Components/Hero/Hero.js
@@ -3,7 +3,7 @@ import "./Hero.css";
 
 export default function Hero(props) {
   const handleDownload = () => {
-    window.location.href = "/Resume2024-Front-EndDeveloper.pdf";
+    window.location.href = "/Resume2024-FrontEndDeveloper.pdf";
   };
 
   const handleEmail = () => {
